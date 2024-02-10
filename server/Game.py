1e7cb1aa2aaac8aabc61cc6e5fee79df4761d0ad
dimaa-b <70352157+dimaa-b@users.noreply.github.com>
2024-02-10 17:43:32 +0000

    socketio initial

--- server/Game.py
+++ server/Game.py
@@ -1,28 +1,29 @@
-    import time
-    import socketio
+import time
+import socketio
 
-    PLAYERS_PER_GAME = 4
-    class Game:
-        def __init__(self, gameID, language, numPlayers):
-            self.id = gameID
-            self.language = language
-            self.numPlayers = numPlayers
-            self.players = []
-            self.gameState = 'waiting'
-            self.round = 0
-            self.gameLoop()
+PLAYERS_PER_GAME = 4
+class Game:
+    def __init__(self, gameID, language, numPlayers):
+        self.id = gameID
+        self.language = language
+        self.numPlayers = numPlayers
+        self.players = []
+        self.gameState = 'waiting'
+        self.round = 0
+        self.gameLoop()
 
     def gameLoop(self):
         # generate the web socket server
         sio = socketio.Server()
         app = socketio.WSGIApp(sio)
+        print("The game has started. Game ID: " + str(self.id))
 
         @sio.event
         def disconnect(sid):
             print('disconnect ', sid)
 
         @sio.event
-        def PlayerAdd(sid, data): 
+        def connect(sid, data): 
             print('connect ', sid)
             username = data['username']  # Assuming username is sent from client
 
@@ -34,9 +35,6 @@
             else:
                 emit('updatePlayers', players, broadcast=True)  # Update player list
 
-        # Actual game logic has to be implemented here (e.g send questions to clients, recieve answers from clients, send timeout requests, etc.)
+    # Actual game logic has to be implemented here (e.g send questions to clients, recieve answers from clients, send timeout requests, etc.)
 
-
-        @sio.event
-
-        
+    
